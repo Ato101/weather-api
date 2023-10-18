78745a04928a44813f5154b8b1554be332ff9868
kenny <dadzieato317@gmail.com>
2023-10-18 22:29:37 +0000

    added year and station

--- main.py
+++ main.py
@@ -22,6 +22,21 @@ def about(station,date):
     }
     return context
 
+@app.route("/api/v1/yearly/<station>/<year>")
+def get_all_year(station,year):
+    filename = '003 data-small/TG_STAID' + str(station).zfill(6)+'.txt'
+    df = pd.read_csv(filename,skiprows=20)
+    df['    DATE'] =df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startwith(str(year))].to_dict()
+    return result
+
+@app.route("/api/v1/<station>/")
+def get_statiom(station,year):
+    filename = '003 data-small/TG_STAID' + str(station).zfill(6)+'.txt'
+    df = pd.read_csv(filename,skiprows=20,parse_dates=['    DATE'])
+    result= df.to_dict(orient='records')
+    return result
+
 
 if __name__=='__main__':
     app.run(debug=True,port=5000)
